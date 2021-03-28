03865b4af96638144f7d8a947f05cdc14b61710c
liorberi2 <81332354+liorberi2@users.noreply.github.com>
2021-03-28 18:41:50 +0300

    Update password.sh

--- password.sh
+++ password.sh
@@ -1,45 +1,45 @@
 #!/bin/bash
-echo "enter the password"
+     echo "enter the password"
 read password
 len="${#password}"
 
 if test $len -ge 10 ; then
 
-   echo "$password" | grep -q [0-9]
+     echo "$password" | grep -q [0-9]
 
 if test $? -eq 0 ; then
 
-   echo "$password" | grep -q [A-Z]
+     echo "$password" | grep -q [A-Z]
 
 if test $? -eq 0 ; then
 
-   echo "$password" | grep -q [a-z]
+     echo "$password" | grep -q [a-z]
 
 if test $? -eq 0 ; then
 
-   echo "$(tput setaf 2)This is an awesome password!"
+     echo "$(tput setaf 2)This is an awesome password!"
 
-else
+   else
 
-   echo "$(tput setaf 1)weak password include lower case char"
+     echo "$(tput setaf 1)weak password include lower case char"
 
-   fi
+fi
  
-else
+   else
 
-   echo "$(tput setaf 1)weak password include capital char"
+     echo "$(tput setaf 1)weak password include capital char"
 
-   fi
+fi
 
-else
+   else
 
-    echo "$(tput setaf 1)please include the numbers in password it is weak password"
+      echo "$(tput setaf 1)please include the numbers in password it is weak password"
 
-    fi
+fi
 
-else
+    else
 
-echo "$(tput setaf 1)password length should be greater than or equal 10 hence weak password"
+      echo "$(tput setaf 1)password length should be greater than or equal 10 hence weak password"
 
-    fi
+fi
 
