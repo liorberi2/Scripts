d38a191f99436dd8414caee27f46c3683dd556aa
liorberi2 <81332354+liorberi2@users.noreply.github.com>
2021-03-28 18:53:59 +0300

    Update part1.ps1

--- part1.ps1
+++ part1.ps1
@@ -1,12 +1,12 @@
-﻿New-Item "c:\mytask.ps1" -force
+New-Item "c:\mytask.ps1" -force  #file creator
 
-$TxtFilePath = "c:\mytask.ps1"
+$TxtFilePath = "c:\mytask.ps1"   #configure TxtFilePath  
 
-Start-Process notepad.exe $TxtFilePath 
+Start-Process notepad.exe $TxtFilePath  # start notepad
   
-Add-Content "c:\mytask.ps1" "I love the bootcamp!"
+Add-Content "c:\mytask.ps1" "I love the bootcamp!" # add contenet to notepad
 
-Get-Content "c:\mytask.ps1" 
+Get-Content "c:\mytask.ps1"  # get contect from the file created
 
   
 
