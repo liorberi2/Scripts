d3c60c05696726d5eef1567d3c98183b0b0763ce
liorberi2 <81332354+liorberi2@users.noreply.github.com>
2021-03-28 22:06:06 +0300

    Update password.sh

--- password.sh
+++ password.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/bin/bash  #Bash hell login
      echo "enter the password"
 read password
 len="${#password}"
 
-if test $len -ge 10 ; then
+if test $len -ge 10 ; then  #password length
 
      echo "$password" | grep -q [0-9]
 
@@ -17,11 +17,11 @@ if test $? -eq 0 ; then
 
 if test $? -eq 0 ; then
 
-     echo "$(tput setaf 2)This is an awesome password!"
+     echo "$(tput setaf 2)This is an awesome password!"  #setaf2 is green
 
    else
 
-     echo "$(tput setaf 1)weak password include lower case char"
+     echo "$(tput setaf 1)weak password include lower case char" #setaf1 is red 
 
 fi
  
@@ -34,7 +34,6 @@ fi
    else
 
       echo "$(tput setaf 1)please include the numbers in password it is weak password"
-
 fi
 
     else
